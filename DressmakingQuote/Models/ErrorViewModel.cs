f2a5df242ce011b20a8e19e92ea5782d45c37e52
sunseehray <sabtirazona@byui.edu>
2024-12-17 11:17:48 +0800

    Add project files.

---
+++ DressmakingQuote/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DressmakingQuote.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
